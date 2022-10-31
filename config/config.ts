9095d749f0d064529bacbc57f7fffc477b2653c0
WZaradzki <mail@wiktorzaradzki.pl>
2022-10-31 12:01:42 +0100

    export

--- config/config.ts
+++ config/config.ts
@@ -1,3 +1,5 @@
 import { config as loadConfig } from "https://deno.land/std@0.161.0/dotenv/mod.ts";
 
-export const config = await loadConfig();
+export const config = await loadConfig({
+    export: true
+});
